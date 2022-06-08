4df9fd3f73032c0b0771cf288c5c32e65a4831bf
SRS <stian.schikora@nav.no>
2022-06-08 13:36:04 +0200

    Flow completer with conditional record refresh

--- force-app/main/default/aura/fcFlowCompleter/fcFlowCompleterHelper.js
+++ force-app/main/default/aura/fcFlowCompleter/fcFlowCompleterHelper.js
@@ -1,5 +1,10 @@
 ({
-    helperMethod : function() {
+    refreshRecord: function(component) {
+        const refresh = component.get('v.refreshRecord');
 
+        if (refresh === true) {
+            //fire refresh event
+            $A.get('e.force:refreshView').fire();
+        }
     }
-})
+});
